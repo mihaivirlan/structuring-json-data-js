bed64d399f6401096877e9d29b48ac46a751e223
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-14 23:08:44 +0300

    Load array JSON for presidents

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 //JavaScript Document
+var xhr = new XMLHttpRequest();
+xhr.open('GET', "data.json", true);
+xhr.responseType = 'text';
+xhr.send();
+
+xhr.onload = function() {
+    if (xhr.status === 200) {
+        var myStuff = JSON.parse(xhr.responseText);
+        console.log(myStuff);
+        console.log(myStuff[0]);
+        console.log(myStuff[1].first);
+        console.log(myStuff[2].last);
+    } // end of the if
+} // end of the onload function
