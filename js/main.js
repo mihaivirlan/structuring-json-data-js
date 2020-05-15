82ff436429a0e484139167ee2cdc61ed6de4dfc9
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-15 21:26:43 +0300

    Load JSON hospitality data

--- js/main.js
+++ js/main.js
@@ -1,35 +1,16 @@
-//JavaScript Document
+// JavaScript Document
+var hotelInfo;
+var details;
 var xhr = new XMLHttpRequest();
 xhr.open('GET', "data.json", true);
 xhr.responseType = 'text';
 xhr.send();
 
+
 xhr.onload = function() {
-    if (xhr.status === 200) {
-        var myStuff = JSON.parse(xhr.responseText);
-        console.log(myStuff);
-
-        for (i = 0; i < myStuff.presidents.length; i++) {
-            console.log(myStuff.presidents[i].first);
-            console.log(myStuff.presidents[i].last);
-            console.log(myStuff.presidents[i].served);
-
-            console.log(myStuff.vicepresidents[i].first);
-            console.log(myStuff.vicepresidents[i].last);
-        } // end loop
-
-        var myString = "";
-        for (i = 0; i < myStuff.presidents.length; i++) {
-            var x = i + 1;
-            myString += "<br>President "+ x +" was ";
-            myString += myStuff.presidents[i].first + " ";
-            myString += myStuff.presidents[i].last + " ";
-
-            myString += "He served from " + myStuff.presidents[i].served + " with ";
-            myString += myStuff.vicepresidents[i].first + " ";
-            myString += myStuff.vicepresidents[i].last + " ";
-        } // end loop
-        document.getElementById('message').innerHTML = myString;
-        // document.getElementById('message').innerHTML = myStuff.presidents[0].last;
-    } // end of the if
-} // end of the onload function
+    if(xhr.status === 200) {
+        hotelInfo = JSON.parse(xhr.responseText);
+        console.log(hotelInfo);
+  
+    } // end if
+} // end function
