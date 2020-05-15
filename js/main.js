2b3d7a3e9d0bde7d8d2f8edade459c0dcb11632e
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-15 16:18:46 +0300

    Load named JSON for presidents

--- js/main.js
+++ js/main.js
@@ -8,8 +8,5 @@ xhr.onload = function() {
     if (xhr.status === 200) {
         var myStuff = JSON.parse(xhr.responseText);
         console.log(myStuff);
-        console.log(myStuff[0]);
-        console.log(myStuff[1].first);
-        console.log(myStuff[2].last);
     } // end of the if
 } // end of the onload function
